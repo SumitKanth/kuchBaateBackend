f040416835a67efb1d7b7a44313aa5fcb294914b
sumit <sumitkanth999@gmail.com>
2024-03-06 13:41:06 +0530

    added env

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ import express from 'express';
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express();
+
+dotenv.config()
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -11,7 +14,7 @@ app.use(cors({
     credentials: true
 }))
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = createServer(app);
 
@@ -28,6 +31,10 @@ io.on("connection", (socket) => {
 
     socket.on('user-message', ({msg, name}) => {
         socket.broadcast.emit('all-user-message', {msg, name});
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id)
     })
 })
 

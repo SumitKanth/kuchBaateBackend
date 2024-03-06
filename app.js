7b17b6d468bfe6386f56114b0eb6547bb6a3a57b
sumit <sumitkanth999@gmail.com>
2024-03-06 13:13:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true
+}))
+
+const PORT = 3000;
+
+const server = createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`Socket from server: ${socket.id}`);
+
+    socket.on('user-message', ({msg, name}) => {
+        socket.broadcast.emit('all-user-message', {msg, name});
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send('This is an express app');
+})
+
+
+
+server.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
